ad8856c0ee14f0e60782051ec82f44a6668b38a0
Anupam Bhunia <127290811+4086Anupam@users.noreply.github.com>
2023-08-26 01:42:46 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    int addhar[2] ;
+int i;
+// input
+for (i=0;i<2;i++){
+    printf ("Enter the number \n");
+scanf ("%d",&addhar[i]);}
+// output
+for (i=0;i<2;i++){
+    printf ("%d\n",addhar[i]);}
+return 0;
+
+
+}
